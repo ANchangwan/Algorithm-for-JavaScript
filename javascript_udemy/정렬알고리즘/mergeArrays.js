2b86fb986d7db702035b646753b109e9103ec20f
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2022-11-03 11:42:35 +0900

    Update mergeArrays.js

--- javascript_udemy/정렬알고리즘/mergeArrays.js
+++ javascript_udemy/정렬알고리즘/mergeArrays.js
@@ -1,27 +1,35 @@
 // Merges two already sorted arrays
-function merge(arr1, arr2){
-    let results = [];
-    let i = 0;
-    let j = 0;
-    while(i < arr1.length && j < arr2.length){
-        if(arr2[j] > arr1[i]){
-            results.push(arr1[i]);
-            i++;
-        } else {
-            results.push(arr2[j])
-            j++;
-        }
+function merge(arr1, arr2:number[]) {
+  let results:number[] = [];
+  let i:number = 0;
+  let j:number = 0;
+  while(i < arr1.length && j < arr2.length){
+    if(arr2[j] > arr1[i]){
+      results.push(arr1[i]);
+      i++;
+    }else{
+      results.push(arr2[j]);
+      j++;
     }
-    while(i < arr1.length) {
-        results.push(arr1[i])
-        i++;
-    }
-    while(j < arr2.length) {
-        results.push(arr2[j])
-        j++;
-    }
-    return results;
+  }
+  while(i < arr1.length){
+    results.push(arr1[i]);
+    i++;
+  }
+  while(j < arr2.length){
+    results.push(arr2[j])
+    j++;
+  }
+  return results;
 }
-merge([100,200], [1,2,3,5,6])
 
+function mergeSort(arr:number[]=[]){
+  if(arr.length <=1) return arr;
+  let mid:number[] = Math.floor(arr.length/2);
+  let left:number[] = mergeSort(arr.slice(0, mid));
+  let right:number[] = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([5,1,3,"di",8,7]))
                    
