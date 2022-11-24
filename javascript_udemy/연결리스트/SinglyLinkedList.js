b260b25b931c62268049b935fc014e3a4d1962ab
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2022-11-24 16:16:04 +0900

    Create SinglyLinkedList.js
    push 추가

---
+++ javascript_udemy/연결리스트/SinglyLinkedList.js
@@ -0,0 +1,23 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	push(val) {
+		let newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		}
+		while (this.head !== null) {}
+	}
+}
