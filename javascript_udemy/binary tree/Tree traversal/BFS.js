2d690cb6cd083addf8024c84fe2c8216c63b31c9
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2024-08-09 09:56:18 +0900

    Create BFS.js

---
+++ javascript_udemy/binary tree/Tree traversal/BFS.js
@@ -0,0 +1,15 @@
+function bfs() {
+  var node = this.root
+    , queue = []
+    , data = [];
+  queue.push(node);
+  while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left)
+          queue.push(node.left);
+      if (node.right)
+          queue.push(node.right);
+  }
+  return data;
+}
