155d1c73874e82e8f77d64fa0f012124bdda9041
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2024-08-17 14:04:20 +0900

    Update tree.js

--- javascript_udemy/binary tree/tree.js
+++ javascript_udemy/binary tree/tree.js
@@ -1,57 +1,4 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
-    insert(value) {
-        var newNode = new Node(value);
-        if (!this.root) {
-            this.root = newNode;
-            return this;
-        } else {
-            var current = this.root;
-            while (true) {
-                if (value < current.value) {
-                    if (!current.left) {
-                        current.left = newNode;
-                        return this;
-                    }else{
-                        current = current.left;
-                        // 왼쪽으로 이동 후 null을 만날까지 계속 이동
-                    }
-                } else if(value > current.value){
-                    if(!current.right){
-                        current.right = newNode;
-                        return this;
-                    }else{
-                        current = current.right; 
-                    }
-                }
-            }
-        }
-    }
-    find(value){
-        var current = this.root,found = false;
-        while(current && !found){
-            if(value < current.value){
-                current = current.left;
-            }else if(value > current.value){
-                current = current.right;
-            }else{
-                found = true;
-            }
-            
-            return false;
-        }
-    }
-    class Node {
   constructor(value) {
     this.value = value;
     this.right = null;
@@ -201,7 +148,7 @@ deleteNode(root, value) {
             return root;
         }
     }
- }
+}
 }
 
 var tree = new BinarySearchTree();
@@ -210,6 +157,3 @@ tree.insert(5);
 tree.insert(3);
 
 console.log(tree);
-
-
-}
